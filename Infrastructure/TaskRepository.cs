f38f8a2c53801b95cdc0739ca427ae8f974c436a
beyzanuroner <onerbeyzanurr@gmail.com>
2026-02-05 15:32:56 +0300

    Add TaskRepository for in-memory data access

---
+++ Infrastructure/TaskRepository.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using LayeredConsoleApp.Domain;
+
+namespace LayeredConsoleApp.Infrastructure
+{
+    public class TaskRepository
+    {
+        private readonly List<TaskItem> _tasks = new();
+        private int _nextId = 1;
+
+        public TaskItem Add(string title)
+        {
+            var task = new TaskItem
+            {
+                Id = _nextId++,
+                Title = title,
+                IsCompleted = false
+            };
+
+            _tasks.Add(task);
+            return task;
+        }
+
+
+        public List<TaskItem> GetAll()
+        {
+            return _tasks;
+        }
+
+
+        public TaskItem GetById(int id)
+        {
+            return _tasks.FirstOrDefault(t => t.Id == id);
+        }
+
+
+        public bool Remove(int id)
+        {
+            var task = GetById(id);
+
+            if (task == null)
+            {
+                return false;
+            }
+
+            task.IsCompleted = !task.IsCompleted;
+            return true;
+
+        }
+    }
+}
